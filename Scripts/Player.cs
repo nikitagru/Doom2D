ff90dca5fd53bfba2d8c454c02a055ba21bbff49
nikitagru <50076302+nikitagru@users.noreply.github.com>
2020-09-04 00:46:43 +0500

    Created test level and added player's controller
    Created test level, added player's keybord controller and created player's animation

---
+++ Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Transform player;
+    private Rigidbody2D rigidbody;
+    private BoxCollider2D playerCollider;
+
+    public bool isJumping = false;
+
+    private float walkTime = 0.0f;
+    private float walkCooldown = 0.2f;
+
+    public Player(Transform Player, Rigidbody2D rg, BoxCollider2D collider2D)
+    {
+        if (Player != null)
+        {
+            player = Player;
+            rigidbody = rg;
+            playerCollider = collider2D;
+        }
+    }
+
+    public void MoveRight(float speed)
+    {
+        rigidbody.AddForce(Vector2.right * speed);
+        walkTime = walkCooldown;
+    }
+
+    public void MoveLeft(float speed)
+    {
+        rigidbody.AddForce(Vector2.left * speed);
+    }
+
+    
+    public void Jump(float jumpForce)
+    {
+        if (!isJumping)
+        {
+            rigidbody.AddForce(Vector2.up * jumpForce);
+            isJumping = true;
+        }
+    }
+
+    
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    public Rigidbody2D rigidbody;
+    public Transform player;
+    private Animator animatorController;
+    public BoxCollider2D playerCollider;
+    private Player playerEntity;
+
+    private MoveDirection direction = MoveDirection.Right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animatorController = GetComponent<Animator>();
+        playerCollider = GetComponent<BoxCollider2D>();
+        playerEntity = new Player(player, rigidbody, playerCollider);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerEntity.MoveRight(speed);
+            direction = MoveDirection.Right;
+            if (!playerEntity.isJumping)
+            {
+                animatorController.Play("walk_right");
+            } else
+            {
+                animatorController.Play("jump_right");
+            }
+        } 
+        else if (Input.GetKey(KeyCode.A))
+        {
+            playerEntity.MoveLeft(speed);
+            direction = MoveDirection.Left;
+            if (!playerEntity.isJumping)
+            {
+                animatorController.Play("walk_left");
+                
+            } else
+            {
+                animatorController.Play("jump_left");
+            }
+
+        } else
+        {
+            if (!playerEntity.isJumping) 
+            {
+                if (direction == MoveDirection.Right)
+                {
+                    animatorController.Play("idle_right");
+                } else
+                {
+                    animatorController.Play("idle_left");
+                }
+                
+            }
+                
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            playerEntity.Jump(jumpForce);
+            if (direction == MoveDirection.Right)
+            {
+                animatorController.Play("jump_right");
+            } else
+            {
+                animatorController.Play("jump_left");
+            }
+            
+        }
+
+        if ((Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A)) && !playerEntity.isJumping)
+        {
+            if (direction == MoveDirection.Right)
+            {
+                rigidbody.velocity = new Vector2(1, rigidbody.velocity.y);
+            } else
+            {
+                rigidbody.velocity = new Vector2(-1, rigidbody.velocity.y);
+            }
+            
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "rigidPlace")
+        {
+            playerEntity.isJumping = false;
+        }
+    }
+
+    enum MoveDirection
+    {
+        Left,
+        Right
+    }
+}
