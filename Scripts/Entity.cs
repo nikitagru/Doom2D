73c5abb60b07e7962d01797c82ed690010dfe523
nikitagru <50076302+nikitagru@users.noreply.github.com>
2020-09-07 21:30:07 +0500

    code refactoring

---
+++ Scripts/Entity.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    private Transform player;
+    private Rigidbody2D rigidbody;
+
+    public bool isJumping = false;
+
+
+    public Entity(Transform Player, Rigidbody2D rg)
+    {
+        if (Player != null)
+        {
+            player = Player;
+            rigidbody = rg;
+        }
+    }
+
+    public void MoveRight(float speed)
+    {
+        rigidbody.AddForce(Vector2.right * speed);
+    }
+
+    public void MoveLeft(float speed)
+    {
+        rigidbody.AddForce(Vector2.left * speed);
+    }
+
+    
+    public void Jump(float jumpForce)
+    {
+        if (!isJumping)
+        {
+            rigidbody.AddForce(Vector2.up * jumpForce);
+            isJumping = true;
+        }
+    }
+
+    
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripts/EntityController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    public Rigidbody2D rigidbody;
+    private Animator animatorController;
+    public BoxCollider2D entityCollider;
+    private Entity entity;
+
+    private MoveDirection direction = MoveDirection.Right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animatorController = GetComponent<Animator>();
+        entity = new Entity(transform, rigidbody);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            entity.MoveRight(speed);
+            direction = MoveDirection.Right;
+            if (!entity.isJumping)
+            {
+                animatorController.Play("walk_right");
+            } else
+            {
+                animatorController.Play("jump_right");
+            }
+        } 
+        else if (Input.GetKey(KeyCode.A))
+        {
+            entity.MoveLeft(speed);
+            direction = MoveDirection.Left;
+            if (!entity.isJumping)
+            {
+                animatorController.Play("walk_left");
+                
+            } else
+            {
+                animatorController.Play("jump_left");
+            }
+
+        } else
+        {
+            if (!entity.isJumping) 
+            {
+                if (direction == MoveDirection.Right)
+                {
+                    animatorController.Play("idle_right");
+                } else
+                {
+                    animatorController.Play("idle_left");
+                }
+                
+            }
+                
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            entity.Jump(jumpForce);
+            if (direction == MoveDirection.Right)
+            {
+                animatorController.Play("jump_right");
+            } else
+            {
+                animatorController.Play("jump_left");
+            }
+            
+        }
+
+
+        // Player's breaking 
+        if ((Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A)) && !entity.isJumping)
+        {
+            if (direction == MoveDirection.Right)
+            {
+                rigidbody.velocity = new Vector2(1, rigidbody.velocity.y);
+            } else
+            {
+                rigidbody.velocity = new Vector2(-1, rigidbody.velocity.y);
+            }
+            
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "rigidPlace")
+        {
+            entity.isJumping = false;
+        }
+    }
+
+    enum MoveDirection
+    {
+        Left,
+        Right
+    }
+}
